68ab4efd9b78049fc338c302a9553174d3adcc48
hoyeon <aviationlovers@naver.com>
2025-01-18 19:28:41 +0900

    Fix: 로그인 메서드 변경

--- src/main/java/com/baekyathon/dodam/user/UserController.java
+++ src/main/java/com/baekyathon/dodam/user/UserController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +16,6 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final UserService userService;
 
-    // 회원가입
     @Operation(summary = "회원가입", description = "사용자가 회원가입을 할 수 있는 API")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "회원가입 성공",
@@ -30,7 +30,6 @@ public class UserController {
         return ResponseEntity.ok(newUser);
     }
 
-    // 로그인
     @Operation(summary = "로그인", description = "사용자가 로그인할 수 있는 API")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "로그인 성공",
@@ -38,13 +37,13 @@ public class UserController {
             @ApiResponse(responseCode = "401", description = "로그인 실패: 이메일 또는 비밀번호 오류",
                     content = @io.swagger.v3.oas.annotations.media.Content(mediaType = "application/json"))
     })
-    @GetMapping("/login")
+    @PostMapping("/login")
     public ResponseEntity<?> login(@RequestParam("email") String email, @RequestParam("password") String password) {
         boolean isAuthenticated = userService.authenticateUser(email, password);
         if (isAuthenticated) {
-            return ResponseEntity.ok("로그인 성공");
+            return ResponseEntity.status(HttpStatus.OK).body("로그인 성공");
         } else {
-            return ResponseEntity.status(401).body("로그인 실패");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인 실패");
         }
     }
 }
